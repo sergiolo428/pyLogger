2014918cdecc80bd9dfcf5e855b1b73249552191
sergiolo428 <sergiolo428@gmail.com>
2022-11-22 15:36:54 +0100

    File path updated

--- loggerTest.py
+++ loggerTest.py
@@ -4,17 +4,15 @@ import string
 import datetime
 from xml.dom.pulldom import SAX2DOM
 import os
+import sys
+
+os.chdir(sys.path[0]) # Change to current .py file path
 
 def logger(text):
     now = datetime.datetime.now() # Get current date and time
-    with open("logs.txt","a") as f:
+    with open("./logs.txt","a") as f:
         f.write(str(now) + " ||| " + text + "\n") # Write "now"
 
-if __name__=='__main__':
-    pathWindows="C:\\Python\\pyLogger"
-    pathLinux=""
-
-    os.chdir(pathWindows) #Set curent path
-    
+if __name__=='__main__':    
     text="MyText."
     logger(text)
